12b85e8cfb73f719bd70c9e41ccdcfbc748f7d2a
WebHav-bhardwaj <bhardwajvaibhav2611@gmail.com>
2024-02-29 10:14:23 +0530

    First commit

---
+++ src/main/java/com/example/CRUDbackend/CruDbackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.CRUDbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDbackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CruDbackendApplication.class, args);
+	}
+}
